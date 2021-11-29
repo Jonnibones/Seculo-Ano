29f5e32156fc77148cc84d5bccff34e1531541d0
Jonnibones <js5800672@gmail.com>
2021-11-29 18:11:12 -0300

    Finalizado

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 
+    function SeculoAno($ano)
+    {
+        $seculo = ceil($ano/100);
+        return "O ano é: ".$ano." e o século é: ".$seculo;
+    }
 
-
+    echo SeculoAno(1990);
 
 ?>
